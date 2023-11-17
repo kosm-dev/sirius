0b38307d9456fdbb892e7e224a836ae2bb48171c
kosm-dev <72253360+kosm-dev@users.noreply.github.com>
2023-11-17 11:26:04 +0200

    added section hero, fixed header and animation fade-in

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,27 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
+  headerScroll('.header');
+
+  function headerScroll(headerClass) {
+    const header = document.querySelector(headerClass);
+
+    window.onscroll = () => {
+      if (window.scrollY > 0) {
+        header.classList.add('header--scroll');
+        header.classList.remove('header-special');
+        header.classList.add('fade-in');
+      }
+      else {
+        header.classList.remove('header--scroll');
+        header.classList.add('header-special');
+        header.classList.remove('fade-in');
+      }
+
+    };
+
+    
+
+  }
 
 });
