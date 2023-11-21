01b569ce8c818d7f0de43de31e849b3f0f3cc4f2
kosm-dev <72253360+kosm-dev@users.noreply.github.com>
2023-11-21 16:11:10 +0200

    added user purchase

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
+
   if (document.querySelector('.header') || document.querySelector('.header-special')) {
     headerScroll('.header');
     headerScroll('.header-special');
@@ -19,26 +20,76 @@ document.addEventListener("DOMContentLoaded", () => {
     };
   }
 
-  const swiper = new Swiper('.faq-slider', {
-    loop: true,
-    slidesPerView: 'auto',
-    spaceBetween: 18,
-    grabCursor: 'true',
-    // Navigation arrows
-    navigation: {
-      nextEl: '.faq-slider__arrow-right',
-    },
-    keyboard: {
-      enabled: true,
-      onlyInViewport: false,
-    },
+
+  if (document.querySelector('.faq-slider')) {
+    const faqSlider = new Swiper('.faq-slider', {
+      loop: true,
+      slidesPerView: 'auto',
+      spaceBetween: 18,
+      grabCursor: 'true',
+      navigation: {
+        nextEl: '.faq-slider__arrow-right',
+      },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+
+    });
+
+  }
+
+
+
+  if (document.querySelector('.userpurchase-slider')) {
+    const userpurchaseSlider = new Swiper('.userpurchase-slider', {
+      loop: true,
+      slidesPerView: 'auto',
+      spaceBetween: 70,
+      grabCursor: 'true',
+      navigation: {
+        nextEl: '.userpurchase-slider__arrow-right',
+      },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+
+    });
+
+  }
+
+
+// переписать ввиде функции
+// пофиксить спойлер
+  const spoilerContent = document.querySelectorAll(".spoiler__content");
+
+  spoilerContent.forEach((item) => {
+    let spoilerHead = item.querySelector(".spoiler__head");
+    spoilerHead.addEventListener("click", () => {
+      item.classList.toggle("open");
+
+      let spoilerArrow = item.querySelector(".spoiler__icon"),
+        spoilerDescr = item.querySelector(".spoiler__descr");
+
+      spoilerArrow.classList.toggle("open");
+
+      if (item.classList.contains("open")) {
+        spoilerDescr.style.height = `${spoilerDescr.scrollHeight}px`;
+        spoilerDescr.classList.add('active');
+      } else {
+        spoilerDescr.style.height = "0px";
+        spoilerDescr.classList.remove('active');
+      }
+    });
 
   });
+
 
   // const element = document.querySelector('.user-select');
   // const choices = new Choices(element);
 
-   // dropdowns
+  // dropdowns
   // const dropdowns = document.querySelectorAll('.dropdown');
   // dropdowns.forEach(dropdown => {
   //   const select = dropdown.querySelector('.select'),
