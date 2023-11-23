1de45046006795e03e2e9487494df9ef18881248
kosm-dev <72253360+kosm-dev@users.noreply.github.com>
2023-11-23 23:17:38 +0200

    added user-attackhub and modals

--- app/js/main.js
+++ app/js/main.js
@@ -64,17 +64,30 @@ document.addEventListener("DOMContentLoaded", () => {
 
   }
 
-  const selectRequest = new Choices('.userattackhub__select-request', {
-    searchEnabled: false,
-    allowHTML: true,
-  });
+  if (document.querySelector('.userattackhub__select-request')) {
+    const selectRequest = new Choices('.userattackhub__select-request', {
+      searchEnabled: false,
+      allowHTML: true,
+      itemSelectText: '',
+    });
+  }
 
-  const selectMethod = new Choices('.userattackhub__select-method', {
-    searchEnabled: false,
-    allowHTML: true,
-  });
+  if (document.querySelector('.userattackhub__select-method')) {
+    const selectMethod = new Choices('.userattackhub__select-method', {
+      searchEnabled: false,
+      allowHTML: true,
+      itemSelectText: '',
+    });
+  }
 
+  if (document.querySelector('.userattackhub__select-layer4')) {
+    const selectMethodLayer = new Choices('.userattackhub__select-layer4', {
+      searchEnabled: false,
+      allowHTML: true,
+      itemSelectText: '',
+    });
 
+  }
 
 
   // переписать ввиде функции
@@ -104,48 +117,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   });
 
-
-  // tabs
-  const tabs = document.querySelectorAll('.tab__item'),
-    tabsContent = document.querySelectorAll('.tabcontent'),
-    tabsParent = document.querySelector('.tab__items');
-
-  function hideTabContent() {
-    tabsContent.forEach(item => {
-      item.classList.add('hide');
-      item.classList.remove('show');
-
-    });
-
-    tabs.forEach(item => {
-      item.classList.remove('tab__item--active');
-    });
-  }
-
-  function showTabContent(i = 0) {
-    tabsContent[i].classList.add('show');
-    tabsContent[i].classList.remove('hide');
-    tabs[i].classList.add('tab__item--active');
-  }
-
-  hideTabContent();
-
-  showTabContent();
-  tabsParent.addEventListener('click', (event) => {
-    const target = event.target;
-
-    if (target && target.classList.contains('tab__item')) {
-      tabs.forEach((item, i) => {
-        if (target == item) {
-          hideTabContent();
-
-          showTabContent(i);
-        }
-      });
-    }
-
-
-  });
 
 
 });
