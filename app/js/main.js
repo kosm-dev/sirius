cbe853657be8f057fb4bc3a6bbff3d54bd3f429b
kosm-dev <72253360+kosm-dev@users.noreply.github.com>
2023-11-24 01:34:44 +0200

    added last change

--- app/js/main.js
+++ app/js/main.js
@@ -90,32 +90,82 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
 
+
+  if (document.querySelector('.user-dropdown__head')) {
+    var userDropdownHead = document.querySelector('.user-dropdown__head');
+
+    userDropdownHead.addEventListener('click', function () {
+      userDropdownHead.classList.toggle('active');
+    });
+
+  }
+
+
+
+
+
   // переписать ввиде функции
   // пофиксить спойлер
 
 
-  const spoilerContent = document.querySelectorAll(".spoiler__content");
+  // const spoilerContent = document.querySelectorAll(".spoiler__content");
 
-  spoilerContent.forEach((item) => {
-    let spoilerHead = item.querySelector(".spoiler__head");
-    spoilerHead.addEventListener("click", () => {
-      item.classList.toggle("open");
+  // spoilerContent.forEach((item) => {
+  //   let spoilerHead = item.querySelector(".spoiler__head");
+  //   spoilerHead.addEventListener("click", () => {
+  //     item.classList.toggle("open");
 
-      let spoilerArrow = item.querySelector(".spoiler__icon"),
-        spoilerDescr = item.querySelector(".spoiler__descr");
+  //     let spoilerArrow = item.querySelector(".spoiler__icon"),
+  //       spoilerDescr = item.querySelector(".spoiler__descr");
 
-      spoilerArrow.classList.toggle("open");
+  //     spoilerArrow.classList.toggle("open");
 
-      if (item.classList.contains("open")) {
-        spoilerDescr.style.height = `${spoilerDescr.scrollHeight}px`;
-        spoilerDescr.classList.add('active');
-      } else {
-        spoilerDescr.style.height = "0px";
-        spoilerDescr.classList.remove('active');
-      }
-    });
+  //     if (item.classList.contains("open")) {
+  //       spoilerDescr.style.height = `${spoilerDescr.scrollHeight}px`;
+  //       spoilerDescr.classList.add('active');
+  //     } else {
+  //       spoilerDescr.style.height = "0px";
+  //       spoilerDescr.classList.remove('active');
+  //     }
+  //   });
 
-  });
+  // });
+
+
+  // function toggleSpoiler(item, headClass, iconClass, descrClass) {
+  //   item.classList.toggle("open");
+
+  //   let spoilerArrow = item.querySelector(`.${iconClass}`),
+  //     spoilerDescr = item.querySelector(`.${descrClass}`);
+
+  //   spoilerArrow.classList.toggle("open");
+
+  //   if (item.classList.contains("open")) {
+  //     spoilerDescr.style.height = `${spoilerDescr.scrollHeight}px`;
+  //     spoilerDescr.classList.add("active");
+  //   } else {
+  //     spoilerDescr.style.height = "0px";
+  //     spoilerDescr.classList.remove("active");
+  //   }
+  // }
+
+  // function handleSpoilerClick(item, headClass, iconClass, descrClass) {
+  //   return () => {
+  //     toggleSpoiler(item, headClass, iconClass, descrClass);
+  //   };
+  // }
+
+  // const spoilerContent = document.querySelectorAll(".spoiler__content");
+
+  // spoilerContent.forEach((item) => {
+  //   let spoilerHead = item.querySelector(".spoiler__head"),
+  //     spoilerIcon = item.querySelector(".spoiler__icon"),
+  //     spoilerDescr = item.querySelector(".spoiler__descr");
+
+  //   spoilerHead.addEventListener("click", handleSpoilerClick(item, "spoiler__head", "spoiler__icon", "spoiler__descr"));
+  //   // Можете подставить свои классы вместо "spoiler__head", "spoiler__icon", "spoiler__descr"
+  // });
+
 
 
 
